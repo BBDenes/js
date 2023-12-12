d8dcda2f1d3370383ef88beb9d6768fa754f6271
BBDenes <csennel302@gmail.com>
2023-12-12 22:58:48 +0100

    tidy, small impro

--- darts counter/scripts/menu.js
+++ darts counter/scripts/menu.js
@@ -1,2 +1,22 @@
 const dartboard = document.getElementById('dartboard');
 dartboard.style.display = 'none';
+const menuButtons = document.getElementsByClassName("game_btn");
+
+for (let i = 0; i < menuButtons.length; i++) {
+    const e = menuButtons[i];
+    e.addEventListener("mousedown", ()=>{
+        refreshButtons(i);
+    });
+    
+}
+
+function refreshButtons(ind) {
+    for (let i = 0; i < menuButtons.length; i++) {
+        if (i == ind) {
+            menuButtons[i].classList.add('selected');
+        } else {
+            menuButtons[i].classList.remove('selected');
+            
+        }
+    }
+}
